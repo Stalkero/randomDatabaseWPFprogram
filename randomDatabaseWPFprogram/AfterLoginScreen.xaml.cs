177b5b5de471fdd4dca1e95dd1eb19d49e78ed08
Stalkero <selteab@interia.pl>
2021-07-08 23:07:44 +0200

    Added basic user individual theme changing feature

--- randomDatabaseWPFprogram/AfterLoginScreen.xaml.cs
+++ randomDatabaseWPFprogram/AfterLoginScreen.xaml.cs
@@ -21,10 +21,60 @@ namespace randomDatabaseWPFprogram
     public partial class AfterLoginScreen : Window
     {
         public AfterLoginScreen(int userDatabaseId)
-        { 
+        {
             InitializeComponent();
 
             userID_TextBox.Text += userDatabaseId;
+
+
+            //Get which theme user had set 
+            //Zdobac jaki motyw uzytkownik ma ustawiony 
+            try
+            {
+
+                string serverConnectionString = "server=localhost;user=root;database=lawfirm;port=3306;password=";
+                string themeSelectionQuery = $"SELECT t.theme FROM users u, user_theme_config t WHERE u.id = {userDatabaseId}";
+
+                MySqlConnection sqlConnection = new MySqlConnection(serverConnectionString);
+
+                sqlConnection.Open();
+
+                MySqlCommand sqlCommand = new MySqlCommand(themeSelectionQuery, sqlConnection);
+                object selectedThemeResults = sqlCommand.ExecuteScalar();
+
+                if (selectedThemeResults != null)
+                {
+                    string selectedThemeResultsToString = Convert.ToString(selectedThemeResults);
+
+                    switch (selectedThemeResultsToString)
+                    {
+                        case "dark":
+                            MainScreenGrid.Background = new SolidColorBrush(Color.FromRgb(50,50, 50));
+
+                            sqlConnection.Close();
+                            break;
+
+                        case "white":
+                            MainScreenGrid.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+
+                            sqlConnection.Close();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+
+
+
+
         }
     }
 }
